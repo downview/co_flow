14178c094fed75f2fca9d2ad07b1e5ed85734787
DownView <downview@protonmail.com>
2021-01-03 00:35:56 -0700

    Return Readable Version

--- flow_test.py
+++ flow_test.py
@@ -5,12 +5,12 @@ import json
 
 # Arguments for the request.
 # field1 returns the full name of the stream gauge 
-# field2 is the flow measurment in cubin feet per second (cfs)
+# field2 is the flow measurment in cubic feet per second (cfs)
 # field3 is the abbreviation for the requested stream gauge
 
 field1 = 'stationName'
 field2 = 'measValue'
-field3 = 'PLAHARCO'
+field3 = 'PLAHARCO,PLACHECO,PLAGEOCO,MFKSTMCO,'
 
 base_url = f"https://dwr.state.co.us/"
 
@@ -18,7 +18,16 @@ endpoint_path = f"Rest/GET/api/v2/telemetrystations/telemetrystation/?fields={fi
 
 endpoint= f'{base_url}{endpoint_path}'
 
-r = requests.get(endpoint)
+response = requests.get(endpoint)
 
-print(r.json())
+# Clean up the output to make it readable
 
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+# Only return the Station Name & Stream Flow in cfs
+
+flows  = response.json()['ResultList']
+
+jprint(flows)
